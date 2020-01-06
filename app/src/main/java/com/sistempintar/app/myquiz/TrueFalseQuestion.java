cb5239dbb22f40161d97da108b704c4cf2dab116
dewisafrida <safridads@gmail.com>
2020-01-06 18:43:01 +0700

    fix truefalsequestion
    hasil fix koding pada benar atau salah jawaban quis

--- app/src/main/java/com/sistempintar/app/myquiz/TrueFalseQuestion.java
+++ app/src/main/java/com/sistempintar/app/myquiz/TrueFalseQuestion.java
@@ -5,32 +5,13 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+public class TrueFalseQuestion {
 
-    public static ArrayList<TrueFalseQuestion> parseJSONArray(String jsonObjectArray)
-    {
-        System.out.println(jsonObjectArray);
-        ArrayList<TrueFalseQuestion> questions = new ArrayList<TrueFalseQuestion>();
+    int id;
+    String trueStatement;
+    String falseStatement, falseStatement2, falseStatement3, soal;
+    String explanation;
 
-        try {
-            JSONArray jArray = new JSONArray(jsonObjectArray);
-            if (jArray != null) {
-                JSONObject jObj;
-                for (int i = 0; i < jArray.length(); i++) {
-                    jObj = (JSONObject)jArray.get(i);
-
-                    TrueFalseQuestion tf = new TrueFalseQuestion();
-                    tf.id = Integer.parseInt((String)jObj.get("id"));
-                    tf.trueStatement = (String)jObj.get("true_statement");
-                    tf.falseStatement = (String)jObj.get("false_statement");
-                    tf.falseStatement2 = (String)jObj.get("false_statement2");
-                    tf.falseStatement3 = (String)jObj.get("false_statement3");
-                    tf.explanation = (String)jObj.get("explanation");
-                    tf.soal = (String)jObj.get("soal");
-
-                    questions.add(tf);
-                }
-            }
-        }
         catch(Exception e)
         {
             System.out.println(e);
