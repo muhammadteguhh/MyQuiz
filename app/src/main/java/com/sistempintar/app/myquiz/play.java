f7b2895e8d49fbebc352183b856caa8288772e8f
bayuprasetyo <bayuprasetyo9803@gmail.com>
2020-01-06 18:36:07 +0700

    fix coding play.java
    pembuatan class skor dan atur waktu pada quiz berlangsung

--- app/src/main/java/com/sistempintar/app/myquiz/play.java
+++ app/src/main/java/com/sistempintar/app/myquiz/play.java
@@ -10,8 +10,6 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
